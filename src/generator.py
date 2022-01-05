9cea1f708979dd1ac39101b7d582e551ec78ea1b
Ostentatoire <justin.duc@epitech.eu>
2022-01-05 18:18:56 +0100

    [FIX] Testeur fixed

--- src/generator.py
+++ src/generator.py
@@ -5,7 +5,7 @@ import random
 def main():
     inside = open('test', "w+")
     for i in range(int(sys.argv[1])):
-        char = chr(random.randrange(32, 128))
+        char = chr(random.randrange(32, 125))
         inside.write(char);
     inside.close()
 
